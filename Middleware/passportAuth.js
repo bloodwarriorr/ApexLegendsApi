36476b6e1394c926718f378237a50b141283a00a
bloodwarriorr <kenar529@gmail.com>
2023-02-02 16:09:21 +0200

    ,,

--- Middleware/passportAuth.js
+++ Middleware/passportAuth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://apex-legends-api.vercel.app/google/callback",
+    callbackURL: "http://localhost:5008/google/callback",
     passReqToCallback:true,
     proxy: true
   },
