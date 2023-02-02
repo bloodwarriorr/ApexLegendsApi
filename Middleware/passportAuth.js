18d36f3119260ce57a88fe7cc830928151606322
bloodwarriorr <kenar529@gmail.com>
2023-02-02 13:35:20 +0200

    changed callback url

--- Middleware/passportAuth.js
+++ Middleware/passportAuth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5008/google/callback",
+    callbackURL: "https://apex-legends-api.vercel.app/google/callback",
     passReqToCallback:true
   },
   function(request, accessToken, refreshToken, profile, done) {
