9cbcf84c379fda8560c3c0053a70824687cd6431
bloodwarriorr <kenar529@gmail.com>
2023-02-02 17:09:52 +0200

    import swagger ui to main file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const session=require('express-session')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument=require('./swagger.json')
-
+const options = { customCssUrl: './node_modules/swagger-ui-dist/swagger-ui.css', customSiteTitle: "The Words That I Know API - Swagger" };
 //port
 const PORT = process.env.PORT || 5008;
 //create server
@@ -24,7 +24,8 @@ server.use(helmet()); //more defense
 server.use('/api/legends',require("./controllers/legend_controller"))
 server.use('/',require("./controllers/user_controller"))
 server.use('/uploads', express.static('./uploads'));
-server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
+
 server.get('/logout',(req,res)=>{
     req.logOut()
     req.session.destroy()
